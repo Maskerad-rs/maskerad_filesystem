fae6a74e1d282636dd2c47801696e3637c0285d7
Malkaviel <maxime.capuano2@gmail.com>
2018-01-30 17:45:35 +0100

    Removed some "duplicated" functions (the Filesystem's functions were calling those functions). There were no reasons to duplicate those functions. Only the read and write related functions have been left as it is. The reason is that the Filesystem provides synchronous and asynchronous file read-write functions. The only differences between the synchronous and asynchronous functions is that the Filesystem, for asynchronous functions, use the functions in a Rayon's threadpool as a closure.

--- src/filesystem.rs
+++ src/filesystem.rs
@@ -32,94 +32,9 @@ use remove_dir_all;
 A filesystem must provide the following functionalities :
 - Manipulating file names and paths.
 - open close read write append create files and directory.
-- does a file or directory exists ?. -> Path stuff.
-- get metadata about files. -> File stuff.
 - scan content of directory.
 - asynchronous I/O (streaming music or textures...).
 */
-
-//TODO: Still not sure about the way to create the bufreader and writers, and about the async stuff.
-//TODO: We assume that the &Path given to the functions is an absolute Path.
-
-fn get_absolute_path(path: &Path) -> FileSystemResult<PathBuf> {
-    fs::canonicalize(path).map_err(|io_error| {
-        FileSystemError::from(io_error)
-    })
-}
-
-//Open file at path with options
-fn open_with_options(path: &Path, open_options: &OpenOptions) -> FileSystemResult<File> {
-
-    open_options
-        .to_fs_openoptions()
-        .open(path)
-        .map_err(|io_error| {
-            FileSystemError::from(io_error)
-        })
-}
-
-fn open_as_bufreader(path: &Path) -> FileSystemResult<BufReader<File>> {
-    let buf = open_with_options(path, OpenOptions::new().set_read(true))?;
-    Ok(BufReader::new(buf))
-}
-
-//Open file at path for writing, truncates if file already exist
-fn create_as_bufwriter(path: &Path) -> FileSystemResult<BufWriter<File>> {
-    let buf = open_with_options(path, OpenOptions::new().set_create(true).set_write(true).set_truncate(true))?;
-    Ok(BufWriter::new(buf))
-}
-
-//Open the file at path for appending, creating it if necessary
-fn append_as_bufwriter(path: &Path) -> FileSystemResult<BufWriter<File>> {
-    let buf = open_with_options(path, OpenOptions::new().set_create(true).set_append(true).set_write(true))?;
-    Ok(BufWriter::new(buf))
-}
-
-
-
-
-
-//create directory at path
-fn mkdir(path: &Path) -> FileSystemResult<()> {
-    fs::DirBuilder::new().recursive(true).create(path).map_err(|io_error| {
-        FileSystemError::from(io_error)
-    })
-}
-//remove a file
-fn rm(path: &Path) -> FileSystemResult<()> {
-
-    if path.is_dir() {
-        fs::remove_dir(path).map_err(|io_error| {
-            FileSystemError::from(io_error)
-        })
-    } else {
-        fs::remove_file(path).map_err(|io_error| {
-            FileSystemError::from(io_error)
-        })
-    }
-}
-//remove file or directory and all its contents
-fn rmrf(path: &Path) -> FileSystemResult<()> {
-    if path.exists() {
-        remove_dir_all::remove_dir_all(path).map_err(|io_error| {
-            FileSystemError::from(io_error)
-        })
-    } else {
-        Ok(())
-    }
-}
-
-//Retrieve all file entries in the given directory (recursive).
-fn read_dir(path: &Path) -> FileSystemResult<fs::ReadDir> {
-
-    fs::read_dir(path).map_err(|io_error| {
-        FileSystemError::from(io_error)
-    })
-}
-
-
-
-
 
 
 fn read<T: Read>(bufreader: &mut BufReader<T>, buf: &mut [u8]) -> FileSystemResult<usize> {
@@ -159,48 +74,6 @@ fn write_all<T: Write>(bufwriter: &mut BufWriter<T>, buf: &[u8]) -> FileSystemRe
 }
 
 
-
-
-
-fn async_read<T: Read + Send>(bufreader: &mut BufReader<T>, buf: &mut [u8], thread_pool: &ThreadPool) -> FileSystemResult<usize> {
-    thread_pool.install(|| {
-        read(bufreader, buf)
-    })
-}
-
-fn async_read_to_end<T: Read + Send>(bufreader: &mut BufReader<T>, buf: &mut Vec<u8>, thread_pool: &ThreadPool) -> FileSystemResult<usize> {
-    thread_pool.install(|| {
-        read_to_end(bufreader, buf)
-    })
-}
-
-fn async_read_to_string<T: Read + Send>(bufreader: &mut BufReader<T>, buf: &mut String, thread_pool: &ThreadPool) -> FileSystemResult<usize> {
-    thread_pool.install(|| {
-        read_to_string(bufreader, buf)
-    })
-}
-
-fn async_read_exact<T: Read + Send>(bufreader: &mut BufReader<T>, buf: &mut [u8], thread_pool: &ThreadPool) -> FileSystemResult<()> {
-    thread_pool.install(|| {
-        read_exact(bufreader, buf)
-    })
-}
-
-fn async_write<T: Write + Send>(bufwriter: &mut BufWriter<T>, buf: &[u8], thread_pool: &ThreadPool) -> FileSystemResult<usize> {
-    thread_pool.install(|| {
-        write(bufwriter, buf)
-    })
-}
-
-fn async_write_all<T: Write + Send>(bufwriter: &mut BufWriter<T>, buf: &[u8], thread_pool: &ThreadPool) -> FileSystemResult<()> {
-    thread_pool.install(|| {
-        write_all(bufwriter, buf)
-    })
-}
-
-
-
-
 pub struct FileSystem {
     game_directories: GameDirectories,
 }
@@ -226,95 +99,133 @@ impl FileSystem {
     }
 
     pub fn get_absolute_path(&self, path: &Path) -> FileSystemResult<PathBuf> {
-        get_absolute_path(path)
+        fs::canonicalize(path).map_err(|io_error| {
+            FileSystemError::from(io_error)
+        })
+    }
+
+    //Open file at path with options
+    fn open_with_options(&self, path: &Path, open_options: &OpenOptions) -> FileSystemResult<File> {
+
+        open_options
+            .to_fs_openoptions()
+            .open(path)
+            .map_err(|io_error| {
+                FileSystemError::from(io_error)
+            })
     }
 
     //Open file at path to read
     pub fn open(&self, path: &Path) -> FileSystemResult<BufReader<File>> {
-        open_as_bufreader(path)
+        let buf = self.open_with_options(path, OpenOptions::new().set_read(true))?;
+        Ok(BufReader::new(buf))
     }
 
     //Open file at path for writing, truncates if file already exist
     pub fn create(&self, path: &Path) -> FileSystemResult<BufWriter<File>> {
-        create_as_bufwriter(path)
+        let buf = self.open_with_options(path, OpenOptions::new().set_create(true).set_write(true).set_truncate(true))?;
+        Ok(BufWriter::new(buf))
     }
 
     //Open the file at path for appending, creating it if necessary
     pub fn append(&self, path: &Path) -> FileSystemResult<BufWriter<File>> {
-        append_as_bufwriter(path)
+        let buf = self.open_with_options(path, OpenOptions::new().set_create(true).set_append(true).set_write(true))?;
+        Ok(BufWriter::new(buf))
     }
 
     //create directory at path
     pub fn mkdir(&self, path: &Path) -> FileSystemResult<()> {
-        mkdir(path)
+        fs::DirBuilder::new().recursive(true).create(path).map_err(|io_error| {
+            FileSystemError::from(io_error)
+        })
     }
 
     //remove a file
     pub fn rm(&self, path: &Path) -> FileSystemResult<()> {
-        rm(path)
+
+        if path.is_dir() {
+            fs::remove_dir(path).map_err(|io_error| {
+                FileSystemError::from(io_error)
+            })
+        } else {
+            fs::remove_file(path).map_err(|io_error| {
+                FileSystemError::from(io_error)
+            })
+        }
     }
 
     //remove file or directory and all its contents
     pub fn rmrf(&self, path: &Path) -> FileSystemResult<()> {
-        rmrf(path)
+        if path.exists() {
+            remove_dir_all::remove_dir_all(path).map_err(|io_error| {
+                FileSystemError::from(io_error)
+            })
+        } else {
+            Ok(())
+        }
     }
 
     //Retrieve all file entries in the given directory (recursive).
     pub fn read_dir(&self, path: &Path) -> FileSystemResult<fs::ReadDir> {
-        read_dir(path)
+
+        fs::read_dir(path).map_err(|io_error| {
+            FileSystemError::from(io_error)
+        })
     }
-
-
-
-
-
 
     pub fn read<T: Read>(&self, bufreader: &mut BufReader<T>, buf: &mut [u8]) -> FileSystemResult<usize> {
         read(bufreader, buf)
     }
-
     pub fn read_to_end<T: Read>(&self, bufreader: &mut BufReader<T>, buf: &mut Vec<u8>) -> FileSystemResult<usize> {
         read_to_end(bufreader, buf)
     }
-
     pub fn read_to_string<T: Read>(&self, bufreader: &mut BufReader<T>, buf: &mut String) -> FileSystemResult<usize> {
         read_to_string(bufreader, buf)
     }
-
     pub fn read_file_exact<T: Read>(&self, bufreader: &mut BufReader<T>, buf: &mut [u8]) -> FileSystemResult<()> {
         read_exact(bufreader, buf)
     }
 
+
     pub fn write<T: Write>(&self, bufwriter: &mut BufWriter<T>, buf: &[u8]) -> FileSystemResult<usize> {
         write(bufwriter, buf)
     }
-
     pub fn write_all<T: Write>(&self, bufwriter: &mut BufWriter<T>, buf: &[u8]) -> FileSystemResult<()> {
         write_all(bufwriter, buf)
     }
 
-    fn async_read<T: Read + Send>(&self, bufreader: &mut BufReader<T>, buf: &mut [u8], thread_pool: &ThreadPool) -> FileSystemResult<usize> {
-        async_read(bufreader, buf, thread_pool)
+
+    pub fn async_read<T: Read + Send>(&self, bufreader: &mut BufReader<T>, buf: &mut [u8], thread_pool: &ThreadPool) -> FileSystemResult<usize> {
+        thread_pool.install(|| {
+            read(bufreader, buf)
+        })
+    }
+    pub fn async_read_to_end<T: Read + Send>(&self, bufreader: &mut BufReader<T>, buf: &mut Vec<u8>, thread_pool: &ThreadPool) -> FileSystemResult<usize> {
+        thread_pool.install(|| {
+            read_to_end(bufreader, buf)
+        })
+    }
+    pub fn async_read_to_string<T: Read + Send>(&self, bufreader: &mut BufReader<T>, buf: &mut String, thread_pool: &ThreadPool) -> FileSystemResult<usize> {
+        thread_pool.install(|| {
+            read_to_string(bufreader, buf)
+        })
+    }
+    pub fn async_read_exact<T: Read + Send>(&self, bufreader: &mut BufReader<T>, buf: &mut [u8], thread_pool: &ThreadPool) -> FileSystemResult<()> {
+        thread_pool.install(|| {
+            read_exact(bufreader, buf)
+        })
     }
 
-    fn async_read_to_end<T: Read + Send>(&self, bufreader: &mut BufReader<T>, buf: &mut Vec<u8>, thread_pool: &ThreadPool) -> FileSystemResult<usize> {
-        async_read_to_end(bufreader, buf, thread_pool)
-    }
 
-    fn async_read_to_string<T: Read + Send>(&self, bufreader: &mut BufReader<T>, buf: &mut String, thread_pool: &ThreadPool) -> FileSystemResult<usize> {
-        async_read_to_string(bufreader, buf, thread_pool)
+    pub fn async_write<T: Write + Send>(&self, bufwriter: &mut BufWriter<T>, buf: &[u8], thread_pool: &ThreadPool) -> FileSystemResult<usize> {
+        thread_pool.install(|| {
+            write(bufwriter, buf)
+        })
     }
-
-    fn async_read_exact<T: Read + Send>(&self, bufreader: &mut BufReader<T>, buf: &mut [u8], thread_pool: &ThreadPool) -> FileSystemResult<()> {
-        async_read_exact(bufreader, buf, thread_pool)
-    }
-
-    fn async_write<T: Write + Send>(&self, bufwriter: &mut BufWriter<T>, buf: &[u8], thread_pool: &ThreadPool) -> FileSystemResult<usize> {
-        async_write(bufwriter, buf, thread_pool)
-    }
-
-    fn async_write_all<T: Write + Send>(&self, bufwriter: &mut BufWriter<T>, buf: &[u8], thread_pool: &ThreadPool) -> FileSystemResult<()> {
-        async_write_all(bufwriter, buf, thread_pool)
+    pub fn async_write_all<T: Write + Send>(&self, bufwriter: &mut BufWriter<T>, buf: &[u8], thread_pool: &ThreadPool) -> FileSystemResult<()> {
+        thread_pool.install(|| {
+            write_all(bufwriter, buf)
+        })
     }
 }
 
@@ -331,16 +242,14 @@ mod filesystem_test {
         let filesystem = FileSystem::new(GameInfos::new("test_filesystem_maskerad", "Malkaviel")).expect("Couldn't create FS");
 
         let current_dir_dir_test = filesystem.construct_path_from_root(&RootDir::WorkingDirectory, "dir_test").expect("Could not create current_dir_dir_test PathBuf");
-        /*println!("{:?}", current_dir_dir_test);
-        println!("{:?}", current_dir_dir_test.as_path());
-        panic!();*/
+
         filesystem.mkdir(current_dir_dir_test.as_path()).expect("Could not create dir with current_dir_dir_test as path");
-        assert!(filesystem.exists(&current_dir_dir_test));
+        assert!(current_dir_dir_test.exists());
 
         //user logs
         let user_log_dir_test = filesystem.construct_path_from_root(&RootDir::UserLogRoot, "log_dir_test").expect("Could not create user_log_dir_test");
         filesystem.mkdir(user_log_dir_test.as_path()).expect("Could not create dir with user_log_dir_test as path");
-        assert!(filesystem.exists(&user_log_dir_test));
+        assert!(user_log_dir_test.exists());
 
         let file_test = filesystem.construct_path_from_root(&RootDir::UserLogRoot, "log_dir_test/file_test.txt").expect("Could not create file_test.txt");
         let mut log_dir_bufwriter = filesystem.create(file_test.as_path()).expect("Could not create log_dir_test/file_test.txt");
@@ -350,7 +259,7 @@ mod filesystem_test {
 
         let async_dir = filesystem.construct_path_from_root(&RootDir::UserLogRoot, "async_dir").expect("Could not create async_dir");
         filesystem.mkdir(async_dir.as_path()).expect("Could not create dir with async_dir as path");
-        assert!(filesystem.exists(&async_dir));
+        assert!(async_dir.exists());
         //test async functionalities.
         let thread_pool = Configuration::new().build().expect("Could not create the thread pool.");
         let async_log_dir_test = filesystem.construct_path_from_root(&RootDir::UserLogRoot, "async_dir/async_log_dir_test.txt").expect("Could not create async_log_dir_test");
@@ -372,16 +281,16 @@ mod filesystem_test {
         assert_eq!(lines.next(), None);
 
         //Metadata
-        let file_metadata = filesystem.metadata(async_log_dir_test.as_path()).expect("Couldn't get metadata");
+        let file_metadata = async_log_dir_test.metadata().expect("Couldn't get metadata");
         assert!(file_metadata.is_file());
         assert!(!file_metadata.is_dir());
         assert!(file_metadata.len() > 0);
 
         //remove
         filesystem.rm(async_log_dir_test.as_path()).expect("Couldn't delete the file : async_dir/async_log_dir_test.txt");
-        assert!(!filesystem.exists(&async_log_dir_test));
+        assert!(!async_log_dir_test.exists());
         filesystem.rmrf(current_dir_dir_test.as_path()).expect("Couldn't delete dir");
-        assert!(!filesystem.exists(&current_dir_dir_test));
+        assert!(!current_dir_dir_test.exists());
     }
 
 
